638b44bed3ba0233919d4a32828f45ec6f076e7d
samirparhi-dev <samirparhi@gmail.com>
2024-05-14 18:52:11 +0530

    added validate Directory path

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use regex::Regex;
 use std::fs;
 use std::io::{self, BufRead, Write};
 use std::path::Path;
+use std::path::PathBuf;
 
-fn search_in_file(file_path: &Path, search_pattern: &Regex) -> io::Result<()> {
+fn search_inside_file(file_path: &Path, search_pattern: &Regex) -> io::Result<()> {
     let file = fs::File::open(file_path)?;
     let reader = io::BufReader::new(file);
 
@@ -23,14 +24,14 @@ fn search_in_file(file_path: &Path, search_pattern: &Regex) -> io::Result<()> {
     Ok(())
 }
 
-fn search_recursively(dir: &Path, pattern: &str) -> io::Result<()> {
+fn search_folder_recursively(dir: &Path, pattern: &str) -> io::Result<()> {
     let search_pattern = Regex::new(pattern).expect("Invalid regex pattern");
 
     for entry in WalkDir::new(dir).into_iter().filter_map(Result::ok) {
         let path = entry.path();
 
         if path.is_file() {
-            if let Err(e) = search_in_file(path, &search_pattern) {
+            if let Err(e) = search_inside_file(path, &search_pattern) {
                 eprintln!("Failed to read file {}: {}", path.display(), e);
             }
         }
@@ -46,14 +47,22 @@ fn main() -> io::Result<()> {
     println!("Enter the directory path to search in:");
     io::stdout().flush()?; // Ensure the prompt is displayed
     io::stdin().read_line(&mut dir)?;
-    let dir = dir.trim(); // Remove any trailing newline characters
+    let dir = dir.trim(); 
 
+    let dir_path = PathBuf::from(dir);
+
+    if !dir_path.is_absolute() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Please provide an absolute path for the directory.",
+        ));
+    }
     println!("Enter the search string:");
     io::stdout().flush()?; // Ensure the prompt is displayed
     io::stdin().read_line(&mut pattern)?;
     let pattern = pattern.trim(); // Remove any trailing newline characters
 
-    search_recursively(Path::new(dir), pattern)?;
+    search_folder_recursively(Path::new(dir), pattern)?;
 
     Ok(())
 }
